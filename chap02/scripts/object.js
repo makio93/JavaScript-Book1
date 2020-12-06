049a1b7afd63b8618e413ad122d12350389be4c6
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-12-06 12:01:01 +0900

    Chapter2の2-3内の各サンプルを作成

---
+++ chap02/scripts/object.js
@@ -0,0 +1,3 @@
+var obj = { x:1, y:2, z:3 };
+console.log(obj.x); // 結果：1
+console.log(obj['x']); // 結果：1
---
+++ chap02/scripts/undefined.js
@@ -0,0 +1,4 @@
+var x;
+var obj = { a:12345 };
+console.log(x); // 結果：undefined（値が設定されていない）
+console.log(obj.b); // 結果：undefined（プロパティが存在しない）
